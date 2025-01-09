741d233e2963b59775e8a3d4d371d8b2fc40ad99
Ravi Ambar <ravi.ambar@gmail.com>
2025-01-09 17:09:59 +0530

    updated lamdba to get city list

--- src/yk-get-city-list/index.mjs
+++ src/yk-get-city-list/index.mjs
@@ -1,40 +1,22 @@
-import { DynamoDBClient, QueryCommand } from "@aws-sdk/client-dynamodb";
+import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
 
 const region = process.env.AWS_REGION || "eu-west-1"; // Default region
 const dynamoDB = new DynamoDBClient({ region });
 
 export async function handler(event) {
-  const cityPrefix = event.queryStringParameters?.cityPrefix || "";
-  console.log(event);
-  if (!cityPrefix) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ error: "cityPrefix is required" }),
-    };
-  }
-
-  const params = {
-    TableName: "City", // Ensure the correct table name
-    IndexName: "CityName-index", // Ensure the correct GSI
-    KeyConditionExpression: "CityName begins_with :cityPrefix",
-    ExpressionAttributeValues: {
-      ":cityPrefix": cityPrefix,
-    },
-  };
-
-  console.log(params);
   try {
-    const result = await dynamoDB.send(new QueryCommand(params));
-    console.log("Äfter Result");
-    console.log(result);
-    const cities = result.Items || []; // Safe handling of result.Items
-    console.log(cities);
+    const params = {
+      TableName: "City",
+    };
+
+    const result = await dynamoDB.send(new ScanCommand(params));
+    console.log("Query result:", result);
     return {
       statusCode: 200,
-      body: JSON.stringify(cities),
+      body: JSON.stringify(result.Items || []),
     };
   } catch (error) {
-    console.error("Error querying DynamoDB:", error);
+    console.error("Error:", error);
     return {
       statusCode: 500,
       body: JSON.stringify({ error: "Could not fetch city suggestions" }),
